7a69d74047be0413c97f88d269caf31b73321f14
Gytis Dudas <gytisdudas@gmail.com>
2020-11-14 12:32:09 +0200

    Add tests for parsing of traits from BEAST1 and BEAST2 trees.
    Will highlight changes that break tree parsing.

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -1,10 +1,44 @@
 import unittest
-import imp
+import baltic as bt
 
 class test_parsers(unittest.TestCase):
-    
+
+    def test_beast1_traits(self):
+        print('Testing BEAST v1 trait parsing')
+
+        ll=bt.loadNexus('./data/InfB_HAt_ALLs1.mcc.tree',tip_regex='_([0-9-]+)')
+
+        ll.traverse_tree()
+        print('Test if branches have correct number of traits')
+        assert len(ll.root.traits)==34
+        print('Root has correct number of traits')
+
+        for k in ll.Objects:
+            if k.is_node() and k!=ll.root:
+                assert len(k.traits) in [77,74,71,41]
+            elif k.is_leaf():
+                assert len(k.traits) in [76,73,70]
+
+        print('Branches have correct number of traits')
+
+    def test_beast2_traits(self):
+        print('Testing BEAST v2 trait parsing')
+        ll=bt.loadNexus('./data/MERS_274_sCoal.combinedTyped.mcc.tree')
+
+        ll.traverse_tree()
+        print('Test if branches have correct number of traits')
+        assert len(ll.root.traits)==10
+        print('Root has correct number of traits')
+
+        for k in ll.Objects:
+            if k.is_node() and k!=ll.root:
+                assert len(k.traits)==13
+            elif k.is_leaf():
+                assert len(k.traits) in [9,12]
+
+        print('Branches have correct number of traits')
+
     def test_newick(self):
-        bt = imp.load_source('baltic', '../baltic/baltic.py')
         tree = bt.loadNewick('./data/zika.nwk')
         expected_num_nodes = 564
         assert len(tree.Objects) == expected_num_nodes, 'Newick tree does not contain correct number of nodes. Expected: {}. Observed: {}'.format(expected_num_nodes, len(tree.Objects))
@@ -13,7 +47,6 @@ class test_parsers(unittest.TestCase):
         assert max_height == expected_height, 'Newick tree height is not correct. Expected: {}. Observed: {}'.format(expected_height, max_height)
 
     def test_nexus(self):
-        bt = imp.load_source('baltic', '../baltic/baltic.py')
         tree = bt.loadNexus('./data/2020-04-13_treetime/divergence_tree.nexus')
         tree.treeStats()
         pass
