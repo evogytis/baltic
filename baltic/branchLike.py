bc545859db310d940b53abcb119150eab19aea8f
Gytis Dudas <gytisdudas@gmail.com>
2025-11-21 14:51:42 +0200

    Fix bug where `get_path_to_root` included baltic superroot

--- baltic/branchLike.py
+++ baltic/branchLike.py
@@ -21,7 +21,7 @@ class BranchLike:
         if path is None: path = []
 
         #TODO: make this self.parent.parent or flag the superroot
-        if self.parent is None: # the root doesn't have a parent, so just return the path including the root
+        if self.parent.parent is None: # the root doesn't have a parent, so just return the path including the root // changed to .parent.parent because otherwise this function adds the "super root" - a fake parent to the root actually found in the tree string
             return path + [self]
         else: # if we aren't at the root, recurse
             newPath = path + [self]
