6ccc2f664fd07a5e0691ec170a38d7d63061137e
Barney Potter <barney.potter.24@gmail.com>
2026-04-26 12:37:55 -0400

    Refresh validation tests and tutorial outputs

--- tests/testsuite.py
+++ tests/testsuite.py
@@ -1,15 +1,13 @@
 import unittest
-import importlib.util
-spec = importlib.util.spec_from_file_location("baltic", "baltic/baltic.py")
-bt = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(bt)
+
+import baltic as bt
 
 class test_parsers(unittest.TestCase):
     
     def test_beast1_traits(self):
         print('Testing BEAST v1 trait parsing')
 
-        ll=bt.loadNexus('./tests/data/miniFluB.mcc.tree',tip_regex='_([0-9-]+)')
+        ll = bt.load_nexus('./tests/data/miniFluB.mcc.tree', tipRegex='_([0-9-]+)', treeType='time')
 
         ll.traverse_tree()
         print('Test if branches have correct number of traits')
@@ -26,7 +24,7 @@ class test_parsers(unittest.TestCase):
 
     def test_beast2_traits(self):
         print('Testing BEAST v2 trait parsing')
-        ll=bt.loadNexus('./tests/data/MERS.mcc.tree')
+        ll = bt.load_nexus('./tests/data/MERS.mcc.tree', treeType='time')
 
         ll.traverse_tree()
         print('Test if branches have correct number of traits')
@@ -43,7 +41,7 @@ class test_parsers(unittest.TestCase):
 
     def test_newick(self):
 
-        tree = bt.loadNewick('./tests/data/zika.nwk')
+        tree = bt.load_newick('./tests/data/zika.nwk', treeType='divergence')
         expected_num_nodes = 564
         assert len(tree.Objects) == expected_num_nodes, 'Newick tree does not contain correct number of nodes. Expected: {}. Observed: {}'.format(expected_num_nodes, len(tree.Objects))
         max_height = round(max([i.height for i in tree.Objects]), 4)
@@ -51,8 +49,9 @@ class test_parsers(unittest.TestCase):
         assert max_height == expected_height, 'Newick tree height is not correct. Expected: {}. Observed: {}'.format(expected_height, max_height)
 
     def test_nexus(self):
-
-        tree = bt.loadNexus('./tests/data/2020-04-13_treetime/divergence_tree.nexus')
+        # TreeTime exports this file with an inline tree string but without the
+        # BEAST-style ``tree NAME =`` line that ``load_nexus`` expects.
+        tree = bt.load_newick('./tests/data/2020-04-13_treetime/divergence_tree.nexus', treeType='divergence')
         tree.treeStats()
         pass
 
