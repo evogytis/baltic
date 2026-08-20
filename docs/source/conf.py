a99091d1fe5e39f3615c7d52afdbadbfd572aa1c
Barney Potter <barney.potter.24@gmail.com>
2026-08-20 08:39:23 -0400

    Switch Sphinx docs theme to furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = 'sphinx_rtd_theme'
-# html_theme = 'furo'
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
 html_css_files = ['copybutton.css']
 html_js_files = ['copybutton.js']
